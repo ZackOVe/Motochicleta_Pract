9c7bc000519bc8dd35309d4b96c5425d50db690e
MexiLander <a.23308051281220@cbtis128.edu.mx>
2024-09-24 19:53:20 -0600

    Add files via upload

---
+++ 45.py
@@ -0,0 +1,7 @@
+numero1= input("Introduzca los kilometros recorridos por la motochicleta:")
+kil=int(numero1)
+numero2 = input("Ingrese los litros que consumio: ")
+lit=int(numero2)
+division = kil / lit
+
+print ("El consumo de combustible:" , division)
